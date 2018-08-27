fa62abb0e05f3f8ecd855168c58d822eba62da6b
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-27 19:24:39 +0530

    Quo_Rem

---
+++ cspp2-practice/m1/QuoRem.java
@@ -0,0 +1,20 @@
+/**
+ * Quotient and Remainder.
+ * @author Nehru
+ */
+public final class QuoRem {
+    /**
+     * Quotient and Remainder.
+     */
+    private QuoRem() {
+    }
+    /**
+     * Main Function.
+     * @param args is the parameter function.
+     */
+    public static void main(final String[] args) {
+        int a = 11;
+        int b = 5;
+        System.out.println(a / b + " " + a % b);
+    }
+}
