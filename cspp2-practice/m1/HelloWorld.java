05789e2dfbc953d078d1a4b16f5146268bfe8788
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-27 18:40:37 +0530

    Hello World

---
+++ cspp2-practice/m1/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Empty Constructor.
+ * @author Nehru.
+ */
+public final class HelloWorld {
+    /**
+     * Empty Constructor.
+     */
+    private HelloWorld() {
+    }
+    /**
+     * main function to print the Hello World.
+     * @param args is the parameter for this function.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
