57649708d54d155bb41548ada2bb0f0e50e37cac
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-28 15:37:32 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

---
+++ cspp2_assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		rootsOfQuadraticEquation(a, b, c);
+	}
+	/*
+	Need to write the rootsOfQuadraticEquation function and print the output.
+	*/
+	static void rootsOfQuadraticEquation(int a, int b, int c) {
+		double x = (-(b) + Math.sqrt((b*b) - 4 * a * c))/2 * a;
+		double r = a * (x * x) + b * x + c;
+		double x1 = (-(b) - Math.sqrt((b*b) - 4 * a * c))/2 * a;
+		double r1 = a * (x1 * x1) + b * x1 + c;
+		System.out.println(x + " " + x1);
+	}
+}
