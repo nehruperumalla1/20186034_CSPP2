52e29fb02fd0a980303d3296a502bacc05dd3ac3
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-29 14:21:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2_assignments/m3/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/**
+ * Count of 7 upto n Numbers.
+ * @author Nehru.
+ */
+public class Solution {
+/* Fill the main function to print the number of 7's between 1 to n*/
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments.
+     */
+    String str = "";
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        char num = '7';
+        for (int i = 1; i <= n; i++) {
+            String str = "";
+            str = str + i;
+            for (int k = 0; k < str.length(); k++) {
+            	if(str.charAt(k) == num) {
+            		count++;
+            	}
+            }
+        }
+        System.out.println(count);
+    }
+}
