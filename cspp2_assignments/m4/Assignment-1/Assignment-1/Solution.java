f4fc0502feb3fe1995bae03376dbb9c89eb5455a
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-30 14:48:25 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_assignments/m4/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m4/Assignment-1/Assignment-1/Solution.java
@@ -2,17 +2,21 @@
  * Largest Num in Array.
  * @author     Nehru.
  */
-import java.util.*;
-public class Solution
-{
+import java.util.Scanner;
+public class Solution {
+	/**
+	 * Not using COnstructor.
+	 */
+	private Solution() {
+		//Empty COnstructor.
+	}
 	/**
 	 * { function_description }.
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		int arr[] = new int[n];
 		int high = 0;
 		for (int i = 0; i < n; i++) {
