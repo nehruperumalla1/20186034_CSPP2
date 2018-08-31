49ff4a80fde61ce0a57cbb0315fa410149b03c70
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-31 11:25:01 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

---
+++ cspp2_assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -0,0 +1,50 @@
+/**
+ * Binary to Decimal.
+ * @author Nehru.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Not using Costructor.
+	 */
+	private Solution() {
+		//Empty Constructor.
+	}
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(final String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		for(int i=0;i<=n;i++){
+			String s=sc.next();
+			long res=binaryToDecimal(s);//Write binaryToDecimal function
+			System.out.println(res);
+		}
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      s     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public static long binaryToDecimal(final String s) {
+		double result = 1;
+		int sum = 0;
+		int base = 2;
+		int k = s.length()-1;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == '1') {
+				result = Math.pow(2, k);
+				sum += result;	
+			}
+			k = k-1;
+		}
+		return sum;
+	}
+
+}
