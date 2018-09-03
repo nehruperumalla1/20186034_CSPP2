6d65ddaf1ee52878b268b18991205b5e2ba468e9
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-03 18:58:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -1,29 +1,40 @@
-import java.util.*;
-class InputValidator
-{
-	/*Write the atrributes and methods for InputValidator*/
-	private String data;
-	private String length;
-	public InputValidator(String input) {
-		this.data = input;
-	}
-	public boolean validateData(String inputdata) {
-		if ((this.data).length() >= 6) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-}
-public class Solution
-{
-	public static void main(String args[])
-    {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
-    	System.out.println(i.validateData(input));
-
+/**
+ * Size of String min 6.
+ * @author Nehru.
+ */
+import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
+class InputValidator {
+    /*Write the atrributes and methods for InputValidator*/
+    private String data;
+    private String length;
+    InputValidator(String input) {
+        this.data = input;
     }
-
+    public boolean validateData(String inputdata) {
+        if ((this.data).length() >= 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+/**
+ * Class for solution.
+ */
+public final class Solution
+{
+    /**
+     * Main Class.
+     * @param args Arguments.
+     */
+    public static void main(final String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);       
+        System.out.println(i.validateData(input));
+    }
 }
