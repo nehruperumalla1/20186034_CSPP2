c5d4b5cc105ee95ce0289a763cc47f1a6b97f773
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-03 19:04:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -7,26 +7,53 @@ import java.util.Scanner;
  * Class for input validator.
  */
 class InputValidator {
+    private final int VALUE = 6;
     /**
      * { var_description }.
      */
     private String data;
     private String length;
+    /**
+     * Constructs the object.
+     *
+     * @param      input  The input
+     */
     InputValidator(final String input) {
         this.data = input;
     }
+    /**
+     * { function_description }.
+     *
+     * @param      inputdata  The inputdata
+     *
+     * @return     { description_of_the_return_value }
+     */
     public boolean validateData(final String inputdata) {
-        if ((this.data).length() >= 6) {
+        if ((this.data).length() >= value()) {
             return true;
         } else {
             return false;
         }
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int value() {
+        return VALUE;
     }
 }
 /**
  * Class for solution.
  */
 public final class Solution {
+    /**
+     * Constructor.
+     */
+    private Solution() {
+        //Not using this.
+    }
     /**
      * Main Class.
      * @param args Arguments.
