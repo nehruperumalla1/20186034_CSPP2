b4d49ed71515690f6023e2b090cbb841f2fe1204
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-07 09:49:49 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2_assignments/m11/Assignment-1/Assignment-1/List.java
+++ cspp2_assignments/m11/Assignment-1/Assignment-1/List.java
@@ -253,7 +253,7 @@ public class List {
     {
     // write the logic for subList
     	List l1 = new List();
-    	if ((start >= 0 && start < indexx) && (end-1 < indexx && end > 0)) {
+    	if ((start >= 0 && start < list.length) && (end-1 < list.length && end > 0) && (start > end)) {
     		for (int i = start; i < end; i++) {
     		l1.add(list[i]);
     		}
