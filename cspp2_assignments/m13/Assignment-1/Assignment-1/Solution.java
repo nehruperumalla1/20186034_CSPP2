51c1cc5e7ff750ce257c6227cd7a4c28deaa09db
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-08 11:31:48 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2_assignments/m13/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m13/Assignment-1/Assignment-1/Solution.java
@@ -89,9 +89,9 @@ class Set {
 
     public int[][] cartesianProduct(Set set1) {
     	//System.out.println(set1.size() + " * " + size + " = " + set1.size()*size);
-    	int[][] cartesian = new int[set1.size()*size + 1][2];
+    	int[][] cartesian = new int[set1.size()*size][2];
     	if (set1.size() > 0 && size > 0) {
-    		for (int i = 0; i < cartesian.length; i++) {
+    		for (int i = 0; i < cartesian[0].length; i++) {
     			for (int j = 0; j < 2; i++) {
     				System.out.println(i);
     				cartesian[i][0] = 1;
