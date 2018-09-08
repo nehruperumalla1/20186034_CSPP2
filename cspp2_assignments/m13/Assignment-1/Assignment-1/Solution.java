90776a6a3d458a227cf945b31274692581f93c51
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-08 11:27:36 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2_assignments/m13/Assignment-1/Assignment-1/Solution.java
+++ cspp2_assignments/m13/Assignment-1/Assignment-1/Solution.java
@@ -93,6 +93,7 @@ class Set {
     	if (set1.size() > 0 && size > 0) {
     		for (int i = 0; i < set1.size()*size; i++) {
     			for (int j = 0; j < 2; i++) {
+    				System.out.println(i);
     				cartesian[i][0] = 1;
     				cartesian[i][1] = 0;
     			}
