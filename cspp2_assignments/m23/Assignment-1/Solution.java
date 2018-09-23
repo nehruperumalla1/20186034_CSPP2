8d0430c0be1eecf137a9649d04d4c1e0637d2443
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-23 17:20:22 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2_assignments/m23/Assignment-1/Solution.java
+++ cspp2_assignments/m23/Assignment-1/Solution.java
@@ -121,6 +121,7 @@ public class Solution {
 				String s;
 				while (((s = br.readLine()) != null)) {
 					buffer += s;
+					buffer += " ";
 				}
 				Pattern pat = Pattern.compile("[^a-z A-Z 0-9]");
 				Matcher m = pat.matcher(buffer);
